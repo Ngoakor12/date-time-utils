0ccbd43be070c01b2f4146420eff669372afaffa
ngoakor12 <ngoakor12@gmail.com>
2022-12-05 05:34:51 +0200

    add more functions

--- index.js
+++ index.js
@@ -5,4 +5,36 @@ function createUCTDate(date, time) {
   return newDate;
 }
 
-module.exports = { createUCTDate };
+function convertUTCDateToLocalDate(date) {
+  const newDate = new Date(date);
+  newDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  return newDate;
+}
+
+function convertDateToISOString(date) {
+  const newDate = date.toISOString();
+  return newDate;
+}
+
+function getCurrentUCTDate() {
+  const date = new Date();
+  return date;
+}
+
+function getTimeFromDate(ISODateStr) {
+  return ISODateStr.substr(11, 5);
+}
+
+function todayLocaleDateString() {
+  const d = new Date();
+  return d.toLocaleDateString();
+}
+
+module.exports = {
+  createUCTDate,
+  convertUTCDateToLocalDate,
+  convertDateToISOString,
+  getCurrentUCTDate,
+  getTimeFromDate,
+  todayLocaleDateString,
+};
