22b13fefbb6722500be656bfde7447640664683f
ngoakor12 <ngoakor12@gmail.com>
2022-12-17 20:05:16 +0200

    check parameters are defined before operations

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 function createUCTDate(date, time) {
-  const [month, day, year] = date.split("-");
-  const [hours, seconds] = time.split(":");
-  const newDate = new Date(year, month - 1, day, hours, seconds);
-  return newDate;
+  if (date && time) {
+    const [month, day, year] = date.split("-");
+    const [hours, seconds] = time.split(":");
+    const newDate = new Date(year, month - 1, day, hours, seconds);
+    return newDate;
+  }
 }
 
 function convertUTCDateToLocalDate(date) {
-  const newDate = new Date(date);
-  newDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-  return newDate;
+  if (date) {
+    const newDate = new Date(date);
+    newDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+    return newDate;
+  }
 }
 
 function convertDateToISOString(date) {
-  const newDate = date.toISOString();
-  return newDate;
+  if (date) {
+    const newDate = date.toISOString();
+    return newDate;
+  }
 }
 
 function getCurrentUCTDate() {
@@ -22,41 +28,45 @@ function getCurrentUCTDate() {
 }
 
 function getTimeFromDate(ISODateStr) {
-  return ISODateStr.substr(11, 5);
+  if (ISODateStr) return ISODateStr.substr(11, 5);
 }
 
 function todayLocaleDateString() {
-  const d = new Date();
-  return d.toLocaleDateString();
+  const date = new Date();
+  return date.toLocaleDateString();
 }
 
 function cleanDate(date) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const cleanedDateArray = date.substr(0, 10).split("/");
-  const [day, month, year] = cleanedDateArray;
-  return `${day} ${months[Number(month) - 1]} ${year}`;
+  if (date) {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const cleanedDateArray = date.substr(0, 10).split("/");
+    const [day, month, year] = cleanedDateArray;
+    return `${day} ${months[Number(month) - 1]} ${year}`;
+  }
 }
 
 function getYearMonthDayFromDate(date) {
-  return date.substr(0, 10);
+  if (date) return date.substr(0, 10);
 }
 
 function convertISOStringToDate(date) {
-  const newDate = new Date(date);
-  return newDate;
+  if (date) {
+    const newDate = new Date(date);
+    return newDate;
+  }
 }
 
 module.exports = {
